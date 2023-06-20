29334bad2208f8663f6554e12f88b5b1de7487e3
fatekha <136146103+fatekha@users.noreply.github.com>
2023-06-20 14:56:15 +0700

    Update So_latihan2.bat
    Kelompok3 Puri putri
    (a.b)²+(b-c)²/a-c

--- So_latihan2.bat
+++ So_latihan2.bat
@@ -1,1 +1,14 @@
+@echo off
 
+set /p a=Masukkan Nilai a :
+set /p b=Masukkan Nilai b :
+set /p c=Masukkan Nilai c :
+
+rem Menghitung (a*b)^2+(b-c)^2/(a-c)
+
+set /a hasil=((a*b)*(a*b)+(b-c)*(b-c))/(a-c)
+
+echo -------------------HASIL-----------------------          
+echo Hasil (%a%*%b%)**2+(%b%-%c%)**2/(%a%-%c%):%hasil%
+echo -----------------------------------------------
+pause

9d3dc92b6efea53d203e006371f3db11f3573f50
fatekha <136146103+fatekha@users.noreply.github.com>
2023-07-04 20:21:17 +0700

    Update MenuAplikasidanJaringan.bat

--- MenuAplikasidanJaringan.bat
+++ MenuAplikasidanJaringan.bat
@@ -1,1 +1,22 @@
+@echo off
+:menu
+cls
+echo Menu Aplikasi:
+echo 1. Word
+echo 2. Excel
+echo 3. PowerPoint
+echo.
 
+set /p aplikasi=Pilih aplikasi anda: 
+
+if %aplikasi%==1 (
+    start winword
+) else if %aplikasi%==2 (
+    start excel
+) else if %aplikasi%==3 (
+    start powerpnt
+) else (
+    echo Pilihan tidak valid.
+    pause
+    goto menu
+)

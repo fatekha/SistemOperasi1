6b2d3c9cd5bcecf58ba845e3e4add61b8a7aa304
fatekha <136146103+fatekha@users.noreply.github.com>
2023-07-07 10:15:11 +0700

    Update MenuAplikasidanJaringan.bat

--- MenuAplikasidanJaringan.bat
+++ MenuAplikasidanJaringan.bat
@@ -1,12 +1,13 @@
 @echo off
+title Menu Aplikasi dan Jaringan 
 :menu
 cls
-echo --------------------------------
-echo   Menu Aplikasi:
-echo --------------------------------
-echo 1. Word
-echo 2. Excel
-echo 3. PowerPoint
+echo ----------------------------------
+echo              Menu Aplikasi:
+echo ----------------------------------
+echo    [1.] Word
+echo    [2.] Excel
+echo    [3.] PowerPoint
 echo.
 
 set /p aplikasi=Pilih aplikasi anda: 
@@ -22,3 +23,36 @@ if %aplikasi%==1 (
     pause
     goto menu
 )
+
+:jaringan
+cls
+echo ----------------------------------
+echo                Menu Jaringan:
+echo ----------------------------------
+echo         [1.] Cek IP
+echo         [2.] Ping domain
+echo         [3.] Setting Connection
+echo         [4.] Registry
+echo         [5.] Diagnostic Tool
+echo.
+
+set /p jaringan=Pilih menu jaringan anda: 
+
+if %jaringan%==1 (
+    ipconfig
+) else if %jaringan%==2 (
+    set /p domain="Masukkan nama domain yang akan diping: "
+    ping %domain%
+) else if %jaringan%==3 (
+    start ncpa.cpl
+) else if %jaringan%==4 (
+    start regedit
+) else if %jaringan%==5 (
+    start msdt.exe /id NetworkDiagnosticsNetworkAdapter
+) else (
+    echo Pilihan tidak valid.
+    pause
+    goto jaringan
+)
+
+pause
